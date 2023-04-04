7ff4ab7b66a1f2ae872e2aaf3fa972848739025b
Sweta <swetaagrawal71099@gmail.com>
2023-04-05 00:37:00 +0530

    frontend till password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -37,6 +37,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "user",
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
   resetPasswordToken: String,
   resetPasswordExpire: Date,
 });
@@ -72,7 +77,7 @@ userSchema.methods.getResetPasswordToken = function () {
     .update(resetToken)
     .digest("hex");
 
-  this.resetPasswordExpire = Date.now() + 15*60*1000;
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
 
   return resetToken;
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,63 @@
 import "./App.css";
-import Header from "./component/layout/Header/Header.js";
+import Header from "./component/layout/Header/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import WebFont from "webfontloader";
 import React from "react";
-import Footer from "./component/layout/Footer/Footer.js";
-import Home from "./component/Home/Home.js";
+import Footer from "./component/layout/Footer/Footer";
+import Home from "./component/Home/Home";
 import Loader from "./component/layout/Loader/Loader";
-import ProductDetails from "./component/Product/ProductDetails.js";
-import Products from "./component/Product/Products.js";
-import Search from "./component/Product/Search.js";
+import ProductDetails from "./component/Product/ProductDetails";
+import Products from "./component/Product/Products";
+import Search from "./component/Product/Search";
 import LoginSignUp from "./component/User/LoginSignUp";
+import store from "./store";
+import { loadUser } from "./actions/userAction";
+import UserOptions from "./component/layout/Header/UserOptions";
+import { useSelector } from "react-redux";
+import Profile from "./component/User/Profile";
+import ProtectedRoute from "./component/Route/ProtectedRoute";
+import UpdateProfile from "./component/User/UpdateProfile";
+import UpdatePassword from "./component/User/UpdatePassword";
+import ForgotPassword from "./component/User/ForgotPassword";
+import ResetPassword from "./component/User/ResetPassword.js";
+//removed .js from all of the above ones to make it look cool
+//but if you have error, please add it
 
 function App() {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+
   React.useEffect(() => {
     WebFont.load({
       google: {
         families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
+
+    store.dispatch(loadUser());
   }, []);
 
   return (
     <Router>
       <Header />
+      {isAuthenticated && <UserOptions user={user} />}
       <Route exact path="/" component={Home} />
       <Route exact path="/product/:id" component={ProductDetails} />
       <Route exact path="/products" component={Products} />
       <Route path="/products/:keyword" component={Products} />
 
       <Route exact path="/search" component={Search} />
+
+      <ProtectedRoute exact path="/account" component={Profile} />
+      <ProtectedRoute exact path="/me/update" component={UpdateProfile} />
+      <ProtectedRoute
+        exact
+        path="/password/update"
+        component={UpdatePassword}
+      />
+      
+      <Route exact path="/password/forgot" component={ForgotPassword} />
+      <Route exact path="/password/reset/:token" component={ResetPassword} />
+
       <Route exact path="/login" component={LoginSignUp} />
 
       <Footer />
